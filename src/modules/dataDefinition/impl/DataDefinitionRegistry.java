56f3d30d9e468c4284b367c0a27bf59e6cc71cde
Asaf-prog <128684083+Asaf-prog@users.noreply.github.com>
2023-04-16 11:16:22 +0300

    add new DD

--- src/modules/dataDefinition/impl/DataDefinitionRegistry.java
+++ src/modules/dataDefinition/impl/DataDefinitionRegistry.java
@@ -2,13 +2,17 @@ package modules.dataDefinition.impl;
 
 
 import modules.dataDefinition.api.DataDefinition;
+import modules.dataDefinition.impl.List.ListDataDefinition;
 import modules.dataDefinition.impl.number.DoubleDataDefinition;
 import modules.dataDefinition.impl.number.NumberDataDefinition;
 import modules.dataDefinition.impl.relation.RelationDataDefinition;
 import modules.dataDefinition.impl.string.StringDataDefinition;
 
 public enum DataDefinitionRegistry implements DataDefinition {
-    STRING(new StringDataDefinition()),DOUBLE(new DoubleDataDefinition()), RELATION(new RelationDataDefinition()),NUMBER(new NumberDataDefinition());
+    STRING(new StringDataDefinition()),
+    DOUBLE(new DoubleDataDefinition()),
+    RELATION(new RelationDataDefinition()),
+    NUMBER(new NumberDataDefinition()),List(new ListDataDefinition());
     DataDefinitionRegistry(DataDefinition dataDefinition) {
         this.dataDefinition = dataDefinition;
     }
