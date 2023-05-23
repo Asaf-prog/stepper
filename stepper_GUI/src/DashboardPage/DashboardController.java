c65f2b3fbeda32d04bd14db15d6d1bdfa04eb4e6
Saar Cohen <sarrcohen@gmail.com>
2023-05-23 12:20:46 +0300

    gui 2.1

--- stepper_GUI/src/DashboardPage/DashboardController.java
+++ stepper_GUI/src/DashboardPage/DashboardController.java
@@ -3,71 +3,94 @@ package DashboardPage;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javafx.animation.FadeTransition;
-import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.SplitPane;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.util.Duration;
 
 public class DashboardController {
+
     @FXML
-    private SplitPane dashboardSplitPane;
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button historyScreenBtn;
+
+    @FXML
+    private AnchorPane paneForXmlLoad;
+
+    @FXML
+    private Button dashboardScreenBtn;
+
+    @FXML
+    private Button executionScreenBtn;
+
+    @FXML
+    private Button statsScreenBtn;
+
     @FXML
     private Pane sidebarPane;
+
+    @FXML
+    private AnchorPane CurrPane;
+
+    @FXML
+    private Button buyPremiumBtn;
 
     @FXML
     private ImageView sidebarIcon;
 
     @FXML
-    private VBox sidebarButtons;
-    @FXML
-    private VBox sidebarIcons;
+    private AnchorPane mainPane;
 
     @FXML
-    void handleMouseEntered(ActionEvent event) {
-        double newWidth = dashboardSplitPane.getWidth() - sidebarButtons.getWidth();
-        dashboardSplitPane.setLayoutX(newWidth);
-        sidebarButtons.setVisible(true);
-        sidebarButtons.setManaged(true);
+    private VBox sidebarButtons;
+
+    @FXML
+    void showDashboardScreen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showExecutionScreen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showStatsScreen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showSideMenu(ActionEvent event) {
 
     }
 
     @FXML
     void initialize() {
-
-        assert sidebarIcon != null : "fx:id=\"sidebarIcon\" was not injected: check your FXML file 'dashboard.fxml'.";
-        assert sidebarButtons != null : "fx:id=\"sidebarButtons\" was not injected: check your FXML file 'dashboard.fxml'.";
-        assert sidebarIcons != null : "fx:id=\"sidebarIcons\" was not injected: check your FXML file 'dashboard.fxml'.";
-        assert dashboardSplitPane != null : "fx:id=\"mainPane\" was not injected: check your FXML file 'dashboard.fxml'.";
+        assert historyScreenBtn != null : "fx:id=\"historyScreenBtn\" was not injected: check your FXML file 'dashboard.fxml'.";
+        assert paneForXmlLoad != null : "fx:id=\"paneForXmlLoad\" was not injected: check your FXML file 'dashboard.fxml'.";
+        assert dashboardScreenBtn != null : "fx:id=\"dashboardScreenBtn\" was not injected: check your FXML file 'dashboard.fxml'.";
+        assert executionScreenBtn != null : "fx:id=\"executionScreenBtn\" was not injected: check your FXML file 'dashboard.fxml'.";
+        assert statsScreenBtn != null : "fx:id=\"statsScreenBtn\" was not injected: check your FXML file 'dashboard.fxml'.";
         assert sidebarPane != null : "fx:id=\"sidebarPane\" was not injected: check your FXML file 'dashboard.fxml'.";
-        sidebarPane.setVisible(false);
-        FadeTransition transition = new FadeTransition(Duration.seconds(0.5), sidebarPane);
-        transition.setFromValue(1);
-        transition.setToValue(0);
-        transition.play();
-        sidebarIcon.setOnMouseClicked(ShowSideBar());
+        assert CurrPane != null : "fx:id=\"CurrPane\" was not injected: check your FXML file 'dashboard.fxml'.";
+        assert buyPremiumBtn != null : "fx:id=\"buyPremiumBtn\" was not injected: check your FXML file 'dashboard.fxml'.";
+        assert sidebarIcon != null : "fx:id=\"sidebarIcon\" was not injected: check your FXML file 'dashboard.fxml'.";
+        assert mainPane != null : "fx:id=\"mainPane\" was not injected: check your FXML file 'dashboard.fxml'.";
+        assert sidebarButtons != null : "fx:id=\"sidebarButtons\" was not injected: check your FXML file 'dashboard.fxml'.";
+        //set all menu option to unPressable
+        dashboardScreenBtn.setDisable(true);
+        executionScreenBtn.setDisable(true);
+        statsScreenBtn.setDisable(true);
+        historyScreenBtn.setDisable(true);
 
-    }
 
-    private EventHandler<? super MouseEvent> ShowSideBar() {
-        return event -> {
-        TranslateTransition transition = new TranslateTransition(Duration.seconds(0.5), dashboardSplitPane);
-        transition.setToX(dashboardSplitPane.getLayoutX() - sidebarButtons.getWidth());
-        transition.play();
-        sidebarPane.setTranslateX(-176);
-        transition.setOnFinished((e) -> {
-            sidebarPane.setVisible(true);
-            sidebarPane.setManaged(true);
-            TranslateTransition sideBarTransition = new TranslateTransition(Duration.seconds(0.5), sidebarPane);
-            sideBarTransition.setToX(0);
-            sideBarTransition.play();
-        });
-        };
     }
 }
