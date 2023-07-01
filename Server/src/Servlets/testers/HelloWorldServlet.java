0c7d23d764c6cdfad96124b96e550be6fef0f385
Saar Cohen <sarrcohen@gmail.com>
2023-07-01 10:38:00 +0300

    saar pc

---
+++ Server/src/Servlets/testers/HelloWorldServlet.java
@@ -0,0 +1,23 @@
+package Servlets.testers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "Hello Servlet",urlPatterns = "/hello")
+public class HelloWorldServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        resp.getWriter().println( "HELLOOO!");
+        //get context from servlet context
+        getServletContext().setAttribute("name","david");
+        //forward to continue servlet
+        getServletContext().getRequestDispatcher("/continue").forward(req,resp);
+
+
+    }
+}
